e4c4f195b556c80fcdd9ee32fd9af041f7e1f6fc
Songqi Liu <liusqi1992@gmail.com>
2015-04-06 15:06:19 -0700

    initialize project

---
+++ SmaritanWebApi/SmaritanWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SmaritanWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SmaritanWebApi/SmaritanWebApi/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace SmaritanWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
